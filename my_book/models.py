d9e4847f74c0c63242c060928191f587136f2f1b
derek_520 <18700862394@139.com>
2018-02-20 16:08:30 +0800

    创建模型完成

--- my_book/models.py
+++ my_book/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+# 创建模型
+
+#定义图书模型类BookInfo
+class BookInfo(models.Model):
+    # 图书名称
+    btitle = models.CharField(max_length=20)
+    # 日期
+    bdate = models.DateField()
+    # 阅读量
+    bread = models.IntegerField(default=0)
+    # 评论量
+    bcommit = models.IntegerField(default=0)
+    # 逻辑删除
+    is_delete = models.BooleanField(default=False)
+#定义英雄模型类HeroInfo
+class HeroInfo(models.Model):
+    # 英雄姓名
+    hname = models.CharField(max_length=20)
+    # 英雄性别
+    hgender = models.BooleanField(default=True)
+    # 英雄描述
+    hcommit = models.CharField(max_length=255)
+    # 逻辑删除
+    is_delete = models.BooleanField(default=False)
+    # 外键 英雄与图书表的关系为一对多，所以属性定义在英雄模型类中
+    hbook = models.ForeignKey('BookInfo')
